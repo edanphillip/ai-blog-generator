01f0587df7bcc2622217cb373b09692402ac17ee
Edan Phillip <edanphillip@gmail.com>
2024-02-01 16:02:25 +0800

    bug: dashboard redirects even when signed in 2

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -12,16 +12,16 @@ const Page = () => {
   const [loadingPage, setloadingPage] = useState(true)
   const { isSignedIn, isLoaded } = useUser();
   useEffect(() => {
-    if (isSignedIn && isLoaded)
+    if (isLoaded)
       setloadingPage(false)
-  }, [isSignedIn, isLoaded])
+  }, [isLoaded])
 
   //if user/page loading
   if (!isLoaded || loadingPage) { return <div className="flex flex-col justify-center items-center h-screen bg-secondary"><BeatLoader loading color="white" /></div>; }
-  //if user/page loading
-  else if (isSignedIn && isLoaded) { return <Dashboard /> }
+  //if signed in
+  else if (!isSignedIn) { console.error("page loaded but user signed out") }
+  else { return <Dashboard /> }
   //verify signed in stage
-  else { console.error("isSignedIn && !isLoaded") }
   // else { redirect("/login") }
 }
 
