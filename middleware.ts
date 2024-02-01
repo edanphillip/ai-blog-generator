87a29425526905e2db2044ce45d091f572cc66a4
Edan Phillip <edanphillip@gmail.com>
2024-02-02 01:23:46 +0800

    middleware issue fix attempt

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,31 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 
 export default authMiddleware({
   publicRoutes: ["/", "/login", "/register", "/api/webhooks/clerk", "/api/webhooks/stripe", "/register/verify-email-address"],
+  afterAuth(auth, req, evt) {
+    // Handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+    // Redirect logged in users to organization selection page if they are not active in an organization
+    // if (
+    //   auth.userId &&
+    //   !auth.orgId &&
+    //   req.nextUrl.pathname !== "/org-selection"
+    // ) {
+    //   const orgSelection = new URL("/org-selection", req.url);
+    //   return NextResponse.redirect(orgSelection);
+    // }
+    // If the user is logged in and trying to access a protected route, allow them to access route
+    if (auth.userId && !auth.isPublicRoute) {
+      return NextResponse.next();
+    }
+    // Allow users visiting public routes to access them
+    return NextResponse.next();
+  },
 });
 
 export const config = {
