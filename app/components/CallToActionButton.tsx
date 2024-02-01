ee899a408f2dce79e6bb8af8385a863ce265df08
Edan Phillip <edanphillip@gmail.com>
2024-02-02 07:06:01 +0800

    add minor navbar styling before releasing to main

--- app/components/CallToActionButton.tsx
+++ app/components/CallToActionButton.tsx
@@ -15,7 +15,7 @@ const PurchaseTokensButton = ({ route = "/login", className: classname = "", cta
         href={route}
         onMouseOver={() => { setHovering(true) }}
         onMouseLeave={() => { setHovering(false) }}
-        className={"flex flex-row gap-3 hover:bg-primary-900 hover:text-black rounded-md   transition-all duration-300 text-center py-2 pr-4 pl-3 border-b min-w-[100px] min-h-[30px] items-center text-lg border-primary-200/20 text-primary " + classname}>
+        className={"flex flex-row gap-3 bg-accent/40 hover:bg-accent hover:text-black rounded-md   transition-all duration-300 text-center py-2 pr-4 pl-3 border-b min-w-[100px] min-h-[30px] items-center text-lg border-primary-200/20 text-primary " + classname}>
         <Image className={hovering ? "animate-spin" : ""} src={coin.src} alt="coin" height={22} width={22} />
         {!hovering && <>
           {tokens == null && <RingLoader size={18} color="white" />}
--- app/components/CustomNavBarLink.tsx
+++ app/components/CustomNavBarLink.tsx
@@ -8,8 +8,8 @@ export const CustomNavBarLink = ({ route, text }: { route: string, text: string 
     {/* {currentRoute === route && <Link className={"hover:bg-primary-900 hover:text-black rounded-md block transition-all duration-300 text-center py-2 pr-4 pl-3 border-b min-w-[100px] min-h-[30px] items-center text-lg border-primary-200/20 text-accent underline-offset-8 underline "} href={route}  >{text}</Link>}
     {currentRoute !== route && <Link className={"hover:bg-primary-900 hover:text-black rounded-md block transition-all duration-300 text-center py-2 pr-4 pl-3 border-b min-w-[100px] min-h-[30px] items-center text-lg border-primary-200/20 text-primary "} href={route}  >{text}</Link>} */}
     {/* hear me out chrome tab edition */}
-    {currentRoute === route && <Link className={"hover:bg-primary-900 hover:text-black rounded-md block transition-all duration-300 text-center py-2 pr-4 pl-3 border-b min-w-[100px] min-h-[30px] items-center text-lg border-primary-200/20 text-accent underline-offset-8 underline "} href={route}  >{text}</Link>}
-    {currentRoute !== route && <Link className={"hover:bg-primary-900 hover:text-black rounded-md block transition-all duration-300 text-center py-2 pr-4 pl-3 border-b min-w-[100px] min-h-[30px] items-center text-lg border-primary-200/20 text-primary "} href={route}  >{text}</Link>}
+    {currentRoute === route && <Link className={" hover:bg-accent/70   hover:text-black rounded-md block transition-all duration-300 text-center py-2 pr-4 pl-3 border-b min-w-[100px] min-h-[30px] items-center text-lg border-primary-200/20 text-accent underline-offset-8 underline "} href={route}  >{text}</Link>}
+    {currentRoute !== route && <Link className={" hover:bg-accent/70 hover:text-black rounded-md block transition-all duration-300 text-center py-2 pr-4 pl-3 border-b min-w-[100px] min-h-[30px] items-center text-lg border-primary-200/20 text-primary "} href={route}  >{text}</Link>}
   </li>
 }
 export default CustomNavBarLink
